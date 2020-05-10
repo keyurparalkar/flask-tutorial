4d54081a673018ed7b118f5a3a43f93a95abd86e
keyurparalkar <keur.plkar@gmail.com>
2020-05-10 15:25:59 +0530

    Basic Template created for different Authors

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,15 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hola World !"
+    user = {"username":"Keyur Paralkar"}
+    posts = [{"author":"John","body":"This is TEMP Page"}
+             ,{"author":"Wick","body":"Focus, Commitment, and Sheer will !!"}]
+    
+    return render_template('index.html',title="KAP",user=user,posts=posts)
+
 
 
 
