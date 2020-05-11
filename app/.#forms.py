abe4d3c48b01c32ad5babf40cb03725456595b35
keyurparalkar <keur.plkar@gmail.com>
2020-05-11 21:26:33 +0530

    Improved Form Validation on Sign-in page

--- app/.#forms.py
+++
@@ -1,1 +0,0 @@
-keyurparalkar@keyurparalkar-Inspiron-5577.10595:1589171794
--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('username',validators=[DataRequired()])
+    username = StringField('Username',validators=[DataRequired()])
     password = PasswordField('Password',validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign In')
