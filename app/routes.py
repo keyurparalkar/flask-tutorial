b022b27bef1ca032d33d4c57dad7ebeb0cbfd130
keyurparalkar <keur.plkar@gmail.com>
2020-05-26 22:05:52 +0530

    Added user profile page

--- app/routes.py
+++ app/routes.py
@@ -61,3 +61,15 @@ def register():
         return redirect(url_for('login'))
 
     return render_template('register.html',title='Registration',form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author':user.username,'post':'ABC1'},
+        {'author':user.username,'post':'ABC2'}
+    ]
+
+    return render_template('user.html',user=user,posts=posts
+)
